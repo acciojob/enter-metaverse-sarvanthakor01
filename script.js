151e0d3649ac6a9987906ed5343cd3839fb36fac
Sarvan Thakor <97446457+sarvanthakor01@users.noreply.github.com>
2025-02-19 23:48:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const statusText = document.getElementById("status");
+const enterBtn = document.getElementById("enterBtn");
+
+enterBtn.addEventListener("click", () => {
+    statusText.innerHTML = "<h1>Entered Metaverse</h1>"; // Change text dynamically
+});
